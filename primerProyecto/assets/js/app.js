2516d69ff4568305401407f74dd30e3a5bb8cea3
Alex Silva <easgaitan@gmail.com>
2020-05-27 17:14:56 -0500

    Eliminando tweet desde el DOM

--- primerProyecto/assets/js/app.js
+++ primerProyecto/assets/js/app.js
@@ -8,26 +8,43 @@ eventListeners();
 
 function eventListeners(){
     //Cuando se envia el formulario
-    document.querySelector('#formulario').addEventListener('submit',agregarTweet);    
+    document.querySelector('#formulario').addEventListener('submit',agregarTweet);   
+    
+    //borrar tweets
+    listaTweets.addEventListener('click', borrarTweet);
 }
-//funciones
 
- //Añadir tweet del formulario
- function agregarTweet(e) {
-     e.preventDefault();
-     //leer el valor del textarea
-    const tweet = document.getElementById('tweet').value;
-    //Crear boton de eliminar
-    const botonBorrar = document.createElement('a');
-    botonBorrar.classList = 'borrar-tweet';
-    botonBorrar.innerText = 'X';
 
-     //Crear lemento y anañdir contenido a la lista
-    const li = document.createElement('li');
-    li.innerText = tweet;
-    //Añade boton de borrar al tweet
-    li.appendChild(botonBorrar);
-    //Añade el tweet a la lista
-    listaTweets.appendChild(li);
-  
- }
+
+//FUNCIONES
+
+    //Añadir tweet del formulario
+    function agregarTweet(e) {
+        e.preventDefault();
+        //leer el valor del textarea
+        const tweet = document.getElementById('tweet').value;
+        //Crear boton de eliminar
+        const botonBorrar = document.createElement('a');
+        botonBorrar.classList = 'borrar-tweet';
+        botonBorrar.innerText = 'X';
+
+        //Crear lemento y anañdir contenido a la lista
+        const li = document.createElement('li');
+        li.innerText = tweet;
+        //Añade boton de borrar al tweet
+        li.appendChild(botonBorrar);
+        //Añade el tweet a la lista
+        listaTweets.appendChild(li);
+    
+    }
+
+    //Borrar tweet
+    function borrarTweet(e){
+        e.preventDefault();
+        if (e.target.className === 'borrar-tweet') {
+            e.target.parentElement.remove();    
+            alert('tweet eliminado');
+        }
+
+        
+    }
