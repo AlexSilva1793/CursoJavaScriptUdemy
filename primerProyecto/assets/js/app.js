01b0aab96bf1f72ad16775d4be284e3e6e8fbd98
Alex Silva <easgaitan@gmail.com>
2020-05-27 16:07:49 -0500

    Agregar tweet y botón borrar al DOM

--- primerProyecto/assets/js/app.js
+++ primerProyecto/assets/js/app.js
@@ -6,8 +6,28 @@ const listaTweets = document.getElementById('lista-tweets');
 
 eventListeners();
 
-eventListeners(){
+function eventListeners(){
     //Cuando se envia el formulario
-    
+    document.querySelector('#formulario').addEventListener('submit',agregarTweet);    
 }
 //funciones
+
+ //Añadir tweet del formulario
+ function agregarTweet(e) {
+     e.preventDefault();
+     //leer el valor del textarea
+    const tweet = document.getElementById('tweet').value;
+    //Crear boton de eliminar
+    const botonBorrar = document.createElement('a');
+    botonBorrar.classList = 'borrar-tweet';
+    botonBorrar.innerText = 'X';
+
+     //Crear lemento y anañdir contenido a la lista
+    const li = document.createElement('li');
+    li.innerText = tweet;
+    //Añade boton de borrar al tweet
+    li.appendChild(botonBorrar);
+    //Añade el tweet a la lista
+    listaTweets.appendChild(li);
+  
+ }
