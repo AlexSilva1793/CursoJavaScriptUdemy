73aa99fd7826e17ac89bc6c8c748a66afd1b5259
Alex Silva <easgaitan@gmail.com>
2020-05-28 12:55:18 -0500

    Se almacenan tweets en local storage

--- primerProyecto/assets/js/app.js
+++ primerProyecto/assets/js/app.js
@@ -1,50 +1,70 @@
 //Variables
-const listaTweets = document.getElementById('lista-tweets');
-
+const listaTweets = document.getElementById("lista-tweets");
 
 //Event Listeners
 
 eventListeners();
 
-function eventListeners(){
-    //Cuando se envia el formulario
-    document.querySelector('#formulario').addEventListener('submit',agregarTweet);   
-    
-    //borrar tweets
-    listaTweets.addEventListener('click', borrarTweet);
+function eventListeners() {
+  //Cuando se envia el formulario
+  document
+    .querySelector("#formulario")
+    .addEventListener("submit", agregarTweet);
+
+  //borrar tweets
+  listaTweets.addEventListener("click", borrarTweet);
 }
-
-
 
 //FUNCIONES
 
-    //Añadir tweet del formulario
-    function agregarTweet(e) {
-        e.preventDefault();
-        //leer el valor del textarea
-        const tweet = document.getElementById('tweet').value;
-        //Crear boton de eliminar
-        const botonBorrar = document.createElement('a');
-        botonBorrar.classList = 'borrar-tweet';
-        botonBorrar.innerText = 'X';
+//Añadir tweet del formulario
+function agregarTweet(e) {
+  e.preventDefault();
+  //leer el valor del textarea
+  const tweet = document.getElementById("tweet").value;
+  //Crear boton de eliminar
+  const botonBorrar = document.createElement("a");
+  botonBorrar.classList = "borrar-tweet";
+  botonBorrar.innerText = "X";
 
-        //Crear lemento y anañdir contenido a la lista
-        const li = document.createElement('li');
-        li.innerText = tweet;
-        //Añade boton de borrar al tweet
-        li.appendChild(botonBorrar);
-        //Añade el tweet a la lista
-        listaTweets.appendChild(li);
-    
-    }
+  //Crear elemento y anañdir contenido a la lista
+  const li = document.createElement("li");
+  li.innerText = tweet;
+  //Añade boton de borrar al tweet
+  li.appendChild(botonBorrar);
+  //Añade el tweet a la lista
+  listaTweets.appendChild(li);
+  //Añadir a local storage
+  agregarTweetLocalStorage(tweet);
+}
 
-    //Borrar tweet
-    function borrarTweet(e){
-        e.preventDefault();
-        if (e.target.className === 'borrar-tweet') {
-            e.target.parentElement.remove();    
-            alert('tweet eliminado');
-        }
+//Borrar tweet del DOM
+function borrarTweet(e) {
+  e.preventDefault();
+  if (e.target.className === "borrar-tweet") {
+    e.target.parentElement.remove();
+    alert("tweet eliminado");
+  }
+}
 
-        
-    }
+//Agrega tweet a local storage
+function agregarTweetLocalStorage(tweet) {
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();
+  //Añadir nuevo tweet
+  tweets.push(tweet);
+  //Convertir de string a arreglo para local Storage
+  localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+//Obtener tweets de local storage
+function obtenerTweetsLocalStorage() {
+  let tweets;
+  //Revisamos valores de local storage
+  if (localStorage.getItem('tweets') === null) {
+    tweets = [];
+  } else {
+    tweets = JSON.parse(localStorage.getItem('tweets'));
+  }
+  return tweets;
+}
